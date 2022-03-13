988208dde4be9068a356afa91be41025ade159c8
Al Hern <Alhern8542@gmail.com>
2022-03-13 03:32:35 -0500

    refactored (print & time.sleep())

--- order_breakfast.py
+++ order_breakfast.py
@@ -1,36 +1,32 @@
 import time
 
-print("Hello! I am Bob, the Breakfast Bot.")
-time.sleep(2)
-print("Today we have two breakfasts available.")
-time.sleep(2)
-print("The first is waffles with strawberries and whipped cream.")
-time.sleep(2)
-print("The second is sweet potato pancakes with butter and syrup.")
-time.sleep(2)
+#saving space via refactoring (print and time.sleep())
+def print_pause(string):
+    print(string)
+    time.sleep(2)
+
+#Bot introduces itself
+print_pause("Hello! I am Bob, the Breakfast Bot.")
+print_pause("Today we have two breakfasts available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
 while True:
     while True:
-        response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
+        response = input("Please place your order. Would you like waffles or pancakes?\n").lower() #offers 2 choices
         if "waffles" in response:
-            print("Waffles it is!")
+            print_pause("Waffles it is!")
             break
         elif "pancakes" in response:
-            print("Pancakes it is!")
+            print_pause("Pancakes it is!")
             break
         else:
-            print("Sorry, I don't understand.")
-    time.sleep(2)
-    print("Your order will be ready shortly.")
-    time.sleep(2)
+            print_pause("Sorry, we do not serve that at this time.")
+    print_pause("Your order will be ready shortly.")
     choice = input("Would you like to place another order? Please say 'yes' or 'no'.\n")
     if "yes" in choice.lower():
-        print("Very good, I'm happy to take another order.")
-        time.sleep(2)
+        print_pause("Very good, I'm happy to take another order.")
     elif "no" in choice.lower():
-        time.sleep(2)
-        print("OK, Goodbye!")
-        time.sleep(2)
+        print_pause("It was a pleasure to serve you, Goodbye!")
         break
     else:
-        time.sleep(2)
         print("Sorry, I dont understand.")
