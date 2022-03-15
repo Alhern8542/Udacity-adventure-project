a82075d7a85c8069ac0bd08efb9dc6c05e98031d
Al Hern <Alhern8542@gmail.com>
2022-03-15 15:50:56 -0500

    first commit

---
+++ adventure_extension.py
@@ -0,0 +1,5 @@
+creatures = ["crazy pirate","angry troll","wicked witch","ghost","evil fairy","zombie","cute kitten","flat earther"]
+
+super_weapons = ["the spear of destiny","a lightsaber","a M41A pulse-rifle","a rocket launcher","the sword of Ogoroth"]
+
+items = ["dagger","stick","lollipop","pencil","pepper spray"]
